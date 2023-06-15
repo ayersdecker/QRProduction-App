860edf2ede4f863114652a365c5cfcffeb5d8b36
Decker Ayers <ayersdecker@gmail.com>
2023-06-15 12:22:42 -0400

    Finished

---
+++ QRcoder/QRcoder/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace QRcoder;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		QR.Barcode = QRText.Text + " ";
+    }
+}
+
---
+++ QRcoder/QRcoder/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace QRcoder;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
